8b694a7f38734c2d6079a820ff7c76e979e7687d
viniaquino <contato.viniaquino@outlook.com>
2025-01-20 12:04:03 -0300

    style: atualização na estilização do formtema

--- src/components/temas/formtema/FormTema.tsx
+++ src/components/temas/formtema/FormTema.tsx
@@ -93,24 +93,24 @@ function FormTema() {
 
     return (
         <div className="container flex flex-col items-center justify-center mx-auto">
-            <h1 className="text-4xl text-center my-8">
+            <h1 className="text-4xl text-center my-8 font-bold text-white">
                 {id === undefined ? 'Cadastrar Tema' : 'Editar Tema'}
             </h1>
 
             <form className="w-1/2 flex flex-col gap-4" onSubmit={gerarNovoTema}>
-                <div className="flex flex-col gap-2">
+                <div className="flex flex-col gap-2 font-bold text-white">
                     <label htmlFor="descricao">Descrição do Tema</label>
                     <input
                         type="text"
                         placeholder="Descreva aqui seu tema"
                         name='descricao'
-                        className="border-2 border-slate-700 rounded p-2"
+                        className="border-2 border-slate-700 rounded p-2 text-black"
                         value={tema.descricao}
                         onChange={(e: ChangeEvent<HTMLInputElement>) => atualizarEstado(e)}
                     />
                 </div>
                 <button
-                    className="rounded text-slate-100 bg-indigo-400 
+                    className="rounded font-bold text-slate-100 bg-indigo-400 
                                hover:bg-indigo-800 w-1/2 py-2 mx-auto flex justify-center"
                     type="submit">
                     {isLoading ?
